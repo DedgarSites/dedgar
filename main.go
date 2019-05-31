9da90780f188cb20f29db5ec892c05582f6bcc56
Code Maya <ditzyd4sh@gmail.com>
2019-05-30 20:58:03 -0800

    cert updates

--- main.go
+++ main.go
@@ -11,6 +11,6 @@ func main() {
 	if localPort := os.Getenv("LOCAL_TESTING"); localPort != "" {
 		e.Logger.Info(e.Start(":" + localPort))
 	} else {
-		e.Logger.Info(e.StartTLS(":8443", "/cert/lego/certificates/dashboard.crt", "/cert/lego/certificates/dashboard.key"))
+		e.Logger.Info(e.StartTLS(":8443", "/cert/lego/certificates/dedgar.crt", "/cert/lego/certificates/dedgar.key"))
 	}
 }
