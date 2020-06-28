ad9cbd540744aee25ec2d295cce13c94072e25c7
dedgar <dedgar@shinobu.ninja>
2020-06-27 21:05:10 -0700

    dropping into a new container every 60 days for new certs

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"os"
+	"time"
 
 	"github.com/dedgarsites/dedgar/routers"
 )
@@ -11,6 +13,14 @@ func main() {
 	if localPort := os.Getenv("LOCAL_TESTING"); localPort != "" {
 		e.Logger.Info(e.Start(":" + localPort))
 	} else {
+		go func() {
+			ctx, cancel := context.WithTimeout(context.Background(), 24*60*time.Hour)
+			defer cancel()
+			if err := e.Shutdown(ctx); err != nil {
+				e.Logger.Fatal(err)
+			}
+		}()
+
 		e.Logger.Info(e.StartTLS(":8443", "/cert/lego/certificates/dedgar.crt", "/cert/lego/certificates/dedgar.key"))
 	}
 }
