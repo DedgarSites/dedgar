58b874a5bfbf6619a94b19700c1d1fcbbd3421c3
Doug Edgar <dedgar@redhat.com>
2020-07-06 10:18:23 -0700

    testing with TLS verification

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -2,12 +2,20 @@ package downloader
 
 import (
 	"bytes"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"path"
+)
+
+var (
+	clusterCABundle = "/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt"
 )
 
 type certFile struct {
@@ -16,6 +24,29 @@ type certFile struct {
 
 // FileFromURL downloads file(s) from baseURL and writes it to the specified filePath.
 func FileFromURL(downloadURL, filePath string, fileName ...string) error {
+	insecure := flag.Bool("insecure-ssl", false, "Accept/Ignore all server SSL certificates")
+	flag.Parse()
+
+	rootCAs, _ := x509.SystemCertPool()
+	if rootCAs == nil {
+		rootCAs = x509.NewCertPool()
+	}
+
+	certs, err := ioutil.ReadFile(clusterCABundle)
+	if err != nil {
+		fmt.Printf("Failed to append %q to RootCAs: %v", clusterCABundle, err)
+	}
+
+	if ok := rootCAs.AppendCertsFromPEM(certs); !ok {
+		fmt.Println("No certs appended, using system certs only")
+	}
+
+	config := &tls.Config{
+		InsecureSkipVerify: *insecure,
+		RootCAs:            rootCAs,
+	}
+	tr := &http.Transport{TLSClientConfig: config}
+
 	for _, file := range fileName {
 		var cFile certFile
 
@@ -34,8 +65,10 @@ func FileFromURL(downloadURL, filePath string, fileName ...string) error {
 		req.Header.Set("Content-Type", "application/json")
 		req.Header.Set("Accept", "application/json")
 
-		client := &http.Client{}
+		//client := &http.Client{}
 		//client := &http.Client{Transport: httpClientWithSelfSignedTLS}
+
+		client := &http.Client{Transport: tr}
 
 		resp, err := client.Do(req)
 		if err != nil {
